73c61ccf8149accbed105f27ac94f19e43e9bf69
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-23 14:37:47 +0500

    update.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,18 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const TodoList = ({ todos }) => {
+    const [edit, setEdit] = useState('')
+
+    const onComplete = async () => {
+
+    }
+    const onEdit = async () => { }
+    const onDelete = async () => { }
     return (
         <div className={` flex my-2 p-2 mx-auto rounded ${todos.isCompleted ? 'bg-teal-500' : 'bg-gray-200'}`}>
             <h1 className=' flex flex-grow mt-2 text-2xl font-serif'>{todos}</h1>
-            <button className=' bg-fuchsia-300 text-sm border-2 px-3 rounded-md'>{!todos.isCompleted? "Done" : "Not Done"}</button>
+            <button onClick={onComplete} className=' bg-fuchsia-300 text-sm border-2 px-3 rounded-md'>{!todos.isCompleted ? "Done" : "Not Done"}</button>
             <button className=' bg-green-300 text-sm border-2 px-3 rounded-md'>Edit</button>
             <button className=' bg-red-300 text-sm border-2 px-3 rounded-md'>Delete</button>
         </div>
