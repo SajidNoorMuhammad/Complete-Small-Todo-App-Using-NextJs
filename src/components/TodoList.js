95287b1cfde6f8efb233769c98ac5d7fbeb0e7c5
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-23 14:18:52 +0500

    update and add more work.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const TodoList = ({ todos }) => {
     return (
-        <div className={` rounded ${todos.isCompleted ? 'bg-teal-500' : 'bg-gray-200'}`}>
-            <h1 className=' mt-2 p-3 text-2xl font-serif'>{todos}</h1>
+        <div className={` flex my-2 p-2 mx-auto rounded ${todos.isCompleted ? 'bg-teal-500' : 'bg-gray-200'}`}>
+            <h1 className=' flex flex-grow mt-2 text-2xl font-serif'>{todos}</h1>
+            <button className=' bg-fuchsia-300 text-sm border-2 px-3 rounded-md'>{!todos.isCompleted? "Done" : "Not Done"}</button>
+            <button className=' bg-green-300 text-sm border-2 px-3 rounded-md'>Edit</button>
+            <button className=' bg-red-300 text-sm border-2 px-3 rounded-md'>Delete</button>
         </div>
     )
 }
