846ddb6cc7003ca3beb54e25593f6a9bd9ca1902
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-25 01:05:03 +0500

    update.

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -6,8 +6,18 @@ export async function addTodos(formData) {
     const todo = formData.get('todo');
     fetch(`${process.env.API_URL}/api/todos`,
         {
-            method: "Post",
+            method: "POST",
             body: JSON.stringify({ todo })
+        }
+    )
+    revalidatePath("/todos")
+}
+
+export async function updateTodo(obj) {
+    fetch(`${process.env.API_URL}/api/todos`,
+        {
+            method: "PUT",
+            body: JSON.stringify(obj)
         }
     )
     revalidatePath("/todos")
